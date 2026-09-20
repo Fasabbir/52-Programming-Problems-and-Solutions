c3b84b9aee7927a71aed429379d9b9cbd36d140c
Fasabbir Ahamed Fahad <mdfasabbir1127@gmail.com>
2026-09-20 15:03:53 +0600

    Update Problem 7

--- Problem_7.c
+++ Problem_7.c
@@ -1,5 +1,28 @@
-#include<stdio.h>
-int main(){
-    int T,num;
-    
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int i, j, n;
+    char num[1000];
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+    {
+        int cnt = 1;
+
+        getchar();
+        fgets(num, sizeof(num), stdin);
+
+        for(j = 0; num[j] != '\0'; j++)
+        {
+            if(num[j] == ' ' && num[j + 1] != ' ')
+                cnt++;
+        }
+
+        printf("%d\n", cnt);
+    }
+
+    return 0;
 }
